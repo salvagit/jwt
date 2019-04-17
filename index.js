02d6370af84b1340747ba4a3ac84571873ff11e6
salva <s@salva.io>
2019-04-17 00:52:46 -0300

    install jwt

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = process.env.PORT || 8888;
 
+const users = [
+  { id: 1, username: "admin", password: "admin" },
+  { id: 2, username: "guest", password: "guest" }
+];
+
 app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
-  const user = req.body.username;
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("You need a user name and passwd");
+    return;
+  }
 
-  res.status(200).send(`You logged in with ${user}`);
+  const user = users.find(u => {
+    return u.username === req.body.username && u.password === req.body.password;
+  });
+
+  if (!user) {
+    res.status(401).send("User not found");
+  }
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      username: user.username
+    },
+    "mysupersecretkey",
+    { expiresIn: "3 hours" }
+  );
+
+  res.status(200).send({ access_token: token });
 });
 
 app.get("/status", (req, res) => {
